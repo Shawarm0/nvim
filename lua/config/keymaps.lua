6ecc821c6d00dddb52927c866f71da0bf2ab86ef
Lovin Sharma <loving.shawarma@gmail.com>
2024-10-19 05:08:44 +0100

    getting the fuzzy finder to work

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
+local builtin = require("telescope.builtin")
+
+
 
 -- This is to open neotree
 vim.keymap.set('n', '<C-n>', '<cmd>:Neotree filesystem reveal left<cr>')
+vim.keymap.set('n', '<C-g>', builtin.git_commits)
