720a43de86075a733abcf32577af8decbf1f8bcf
Lovin Sharma <loving.shawarma@gmail.com>
2024-10-24 22:27:11 +0100

    autocomplete with luasnip and lsp snippets working

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,62 @@
+return {
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			-- Set up nvim-cmp.
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = {
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(1) then
+							luasnip.jump(1) -- Jump to the next placeholder
+						elseif cmp.visible() then
+							cmp.select_next_item() -- Select the next item in the completion menu
+						else
+							fallback() -- Fallback to the default Tab behavior
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1) -- Jump to the previous placeholder
+						elseif cmp.visible() then
+							cmp.select_prev_item() -- Select the previous item in the completion menu
+						else
+							fallback() -- Fallback to the default Shift-Tab behavior
+						end
+					end, { "i", "s" }),
+				},
+				sources = cmp.config.sources({
+				  { name = 'nvim_lsp' },
+          { name = "luasnip" }, -- For luasnip users.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
+}
--- lua/plugins/language-server.lua
+++ lua/plugins/language-server.lua
@@ -1,31 +1,35 @@
 return {
-	--  Neovim plugin that allows you to easily install lsp's
-	{
-    	"williamboman/mason.nvim",
-	config = function()
-	require("mason").setup()
-    	end,
-	},
+  --  Neovim plugin that allows you to easily install lsp's
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
 
-	-- Allows for mason and nvim.lsp to talk to each other 
-	{
-	"williamboman/mason-lspconfig.nvim",
-	config = function()
-	require("mason-lspconfig").setup({
-		ensure_installed = { "lua_ls", "quick_lint_js", "pyright" },
-		})
-	end,
-	},
+  -- Allows for mason and nvim.lsp to talk to each other
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "quick_lint_js", "pyright" },
+      })
+    end,
+  },
 
   -- Allows for lsp integration within nvim
-	{
-	"neovim/nvim-lspconfig",
-	config = function()
-	local lsp = require("lspconfig")
-	-- These are my server setups
-	lsp.lua_ls.setup({})
-    	lsp.pyright.setup({})
-	end,
-	}
-
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lsp = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- These are my server setups
+      lsp.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lsp.pyright.setup({
+        -- capabilities = capabilities,
+      })
+    end,
+  },
 }
