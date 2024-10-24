16abaf7dbf2e345927dc1715adb8e737cdd28852
Lovin Sharma <loving.shawarma@gmail.com>
2024-10-24 18:58:38 +0100

    Treesitter highlighting now works. vim-options added so relativenumber is true and default tabspace set to 2

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-	"olimorris/onedarkpro.nvim",
+	"navarasu/onedark.nvim",
 	lazy = false, --make sure we load this during startup
 	priority = 1000, -- make sure to load this before everything else
 	config = function()
-		require("onedarkpro").setup({})
+		require("onedark").setup({})
 		vim.cmd("colorscheme onedark")
 	end,
 }
